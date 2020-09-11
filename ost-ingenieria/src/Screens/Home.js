421aaa156cf7705dc3699f6ae46293b34a43cb97
andresgc04 <andresgc1997@outlook.com>
2020-09-10 20:46:27 -0400

    OST-INGV4.1

--- ost-ingenieria/src/Screens/Home.js
+++ ost-ingenieria/src/Screens/Home.js
@@ -3,6 +3,12 @@ import { Navbar, NavDropdown, Nav, Button, Carousel, CardDeck, Card } from 'reac
 import Fade from 'react-reveal/Fade';
 import Zoom from 'react-reveal/Zoom';
 import { SocialIcon } from 'react-social-icons';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -51,6 +57,87 @@ function ControlledCarousel() {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+    animation: 'speed'
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
+
+
+
+ 
+
+
+ 
+    const tileData = [
+    {
+      img: 'https://concepto.de/wp-content/uploads/2018/04/arquitectura-plano-e1523469843305.jpeg',
+      title: 'Image',
+      author: 'author',
+    },
+    {
+      img: 'https://concepto.de/wp-content/uploads/2018/04/arquitectura-plano-e1523469843305.jpeg',
+      title: 'Image',
+      author: 'author',
+    },
+    {
+      img: 'https://concepto.de/wp-content/uploads/2018/04/arquitectura-plano-e1523469843305.jpeg',
+      title: 'Image',
+      author: 'author',
+    },
+    {
+      img: 'https://concepto.de/wp-content/uploads/2018/04/arquitectura-plano-e1523469843305.jpeg',
+      title: 'Image',
+      author: 'author',
+    }
+ ];
+ 
+
+function SingleLineGridList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`}>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
   );
 }
 
@@ -250,9 +337,12 @@ class Home extends Component {
         </section>
 
         <section style={{ background: 'white' }}>
-          <div className="container mt-5">
+          <div className="container mt-3 py-4">
+            <div className="d-flex justify-content-center">
+              <h1 className="display-4 mt-2" style={{ color: 'black' }}>Nuestros Clientes</h1>
+            </div>
             <div>
-
+            <SingleLineGridList/>
             </div>
           </div>
         </section>
