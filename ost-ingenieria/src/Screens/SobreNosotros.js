bcae8620b1660a27e6176dbd75ea19390a09bfc0
andresgc04 <andresgc1997@outlook.com>
2020-09-11 18:17:43 -0400

    OST-INGV5.1

---
+++ ost-ingenieria/src/Screens/SobreNosotros.js
@@ -0,0 +1,3 @@
+import React, { Component, useState } from 'react';
+import { Navbar, NavDropdown, Nav, Button, Carousel, CardDeck, Card, Image } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
