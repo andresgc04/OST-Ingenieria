1f683b65a6726e052f8040b67c688d57a61de8ca
andresgc04 <andresgc1997@outlook.com>
2021-02-22 19:55:44 -0400

    Updates

---
+++ precache-manifest.e10db2b55d46b7ec1491dcf02dbffc20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec096841bd5751add23f071cbfad27f",
+    "url": "/OST-Ingenieria/index.html"
+  },
+  {
+    "revision": "adca3c803af3dfbd0ac6",
+    "url": "/OST-Ingenieria/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "114427b78f51692ec0fc",
+    "url": "/OST-Ingenieria/static/css/main.43920ae4.chunk.css"
+  },
+  {
+    "revision": "adca3c803af3dfbd0ac6",
+    "url": "/OST-Ingenieria/static/js/2.d9217763.chunk.js"
+  },
+  {
+    "revision": "9f36583424cf900ca3e6c23446e83813",
+    "url": "/OST-Ingenieria/static/js/2.d9217763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "114427b78f51692ec0fc",
+    "url": "/OST-Ingenieria/static/js/main.b0b22bf4.chunk.js"
+  },
+  {
+    "revision": "22081a717798a2dc60b5",
+    "url": "/OST-Ingenieria/static/js/runtime-main.b0a6d3fd.js"
+  }
+]);
